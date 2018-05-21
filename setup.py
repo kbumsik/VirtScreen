99936f6e00424562175dd1247deee4a9e43281e0
Bumsik Kim <kbumsik@gmail.com>
2018-05-20 21:56:17 -0400

    Fix for setup.py versioning

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.0',  # Required
+    version='0.1.1',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -67,7 +67,7 @@ setup(
     #
     # This field corresponds to the "Description-Content-Type" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#description-content-type-optional
-    long_description_content_type='text/markdown',  # Optional (see note above)
+    long_description_content_type='text/markdown; charset=UTF-8; variant=GFM',  # Optional (see note above)
 
     # This should be a valid link to your project's main homepage.
     #
